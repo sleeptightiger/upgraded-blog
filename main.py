90931c74da2762df0fa1a105a9ac53ecdc9743e3
Gerardo Morales2 <gerardo.morales2@ibm.com>
2023-01-10 19:28:19 -0600

    added input form functionality on contact

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-import pprint
+import smtplib
 
 blogs_endpoint = "https://api.npoint.io/6813343cbd4fa3b4db51"
 response = requests.get(blogs_endpoint)
 json = response.json()
 
 app = Flask(__name__)
+
+
+
+MY_EMAIL = "testingtester983@gmail.com"
+MY_PASSWORD = "cbjtqflmssyfuyzb"
 
 @app.route('/')
 def get_index():
@@ -16,9 +21,20 @@ def get_index():
 def get_about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=["POST", "GET"])
 def get_contact():
-    return render_template('contact.html')
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template('contact.html', msg_sent=True)
+    return render_template('contact.html', msg_sent=False)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(MY_EMAIL, MY_EMAIL, email_message)
 
 @app.route("/post/<int:index>")
 def get_post(index):
